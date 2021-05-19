6acf64882175f569f433e6df0b7d7a5234351291
VardaanBansal <79084613+VardaanBansal@users.noreply.github.com>
2021-05-19 09:15:20 +0530

    Add files via upload

---
+++ quize game.js
@@ -0,0 +1,7 @@
+function addUser()
+{
+    player1_name = document.getElementById("player1_name_input").nodeValue;
+    player2_name = document.getElementById("player2_name", player2_name);
+
+    window.location = "game_page.html";
+}
